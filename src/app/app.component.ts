09c4f9ef0374d106b9571320ec47f506364b6e68
fbastianelli <fbastianelli@salesforce.com>
2018-02-20 17:46:30 +0000

    Add on CoinMarketCapApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
 
   coins: String = '';
   marketCapCoins: any[] = [];
+  symbols: any[] = [];
   coinready: boolean = false;
 
   offset = 0;
@@ -61,13 +62,19 @@ export class AppComponent {
       var j = e.text.search(/\$[a-zA-Z]{4}\s/);
 
       if(i > -1){ 
-        if(tmp.filter(function(el){ return el ==  e.text.substring(i+1, i+4) }).length == 0){
-          tmp.push(e.text.substring(i+1, i+4)); }
+        if(tmp.filter(function(el){ return el ==  e.text.substring(i+1, i+4) }).length == 0 &&
+          this.symbols.indexOf(e.text.substring(i+1, i+4)) > -1){
+          //Push e only so you can set the rest in the html and inject new property from marketcap
+          tmp.push(e.text.substring(i+1, i+4)); 
+        }
       }
       //Only first found - usually main topic subject of tweet
       else if(j > -1){ 
-        if(tmp.filter(function(el){ return el ==  e.text.substring(j+1, j+4) }).length == 0){
-          tmp.push(e.text.substring(j+1, j+5)); }
+        if(tmp.filter(function(el){ return el ==  e.text.substring(j+1, j+4) }).length == 0 &&
+          this.symbols.indexOf(e.text.substring(i+1, i+4)) > -1){
+
+          tmp.push(e.text.substring(j+1, j+5)); 
+        }
       }
     });
     this.coins = tmp.join(', ');
@@ -86,6 +93,7 @@ export class AppComponent {
     else{ 
       console.log('Coins set');
       console.log(this.marketCapCoins);
+      this.symbols = this.marketCapCoins.map(function(e){ return e.symbol; });
       this.coinready = true; 
     }
   }
