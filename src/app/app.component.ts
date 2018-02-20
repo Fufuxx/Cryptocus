53c4653b807091b1b3a68679e67634f3c5580dce
fbastianelli <fbastianelli@salesforce.com>
2018-02-20 16:13:59 +0000

    Handle Regex $

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,8 +43,14 @@ export class AppComponent {
     this.tweets.forEach(function(e){
       var i = e.text.search(/\$[a-zA-Z]{3}\s/);
       var j = e.text.search(/\$[a-zA-Z]{4}\s/);
+      console.log('===========');
+      console.log(e.text);
+      console.log('i -> '+i+', j -> '+j);
       if(i > -1){ tmp.push(e.text.substring(i+1, i+4)); }
-      if(j > -1){ tmp.push(e.text.substring(j+1, j+5)); }
+      if(j > -1){ 
+        console.log(e.text.substring(j+1, j+5));
+        tmp.push(e.text.substring(j+1, j+5)); 
+      }
     });
 
     this.coins = tmp.join(', ');
