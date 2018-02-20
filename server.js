e068c48236f4357f7c5ff8a2ef6772364ca29f9e
fbastianelli <fbastianelli@salesforce.com>
2018-02-20 13:55:21 +0000

    init

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+const Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.TOKEN_SECRET
+});
+
 // This gives me the ability to call req.body at any point in order to get the content of the body of any request
 app.use(function(req, res, next){
     var data='';
@@ -17,7 +26,21 @@ app.use(function(req, res, next){
 });
 
 //Handle Routes
-app.use(require('./routes/dummy.js'));
+//app.use(require('./routes/dummy.js'));
+app.get('/tweets', function(req, res, next){
+
+    client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+        if(error){
+            console.log(error);
+            res.json({ error: true, data: error });
+        }
+        else{
+            console.log(tweets);
+            res.json({ error: false, tweets: tweets });
+        }
+    });
+
+});
 
 //Getting static folder
 app.use(express.static(__dirname + '/dist'));
